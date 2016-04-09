33e63f6b270da0ec21d6d5acd3b840447e434023
ali <alidertli25@gmail.com>
2016-04-09 17:54:19 +0300

    veritabanındaki postları görebilmek için template'i değiştirdim.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Ali
+from django.utils import timezone
+
 def post_list(request):
-	return render(request,'blog/post_list.html',{})
+    posts = Ali.objects.filter(yayinlama_tarihi__lte=timezone.now()).order_by('yayinlama_tarihi')
+    return render(request, 'blog/post_list.html', {'posts': posts})
